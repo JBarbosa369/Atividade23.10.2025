899daa82a972cc5255b8f8aa18a2027ab70fc43c
JBarbosa369 <jeovana.nascimento@aluno.senai.br>
2025-10-23 09:31:37 -0500

    atividade feita hoje dia 23.10.2025

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ console.log(listadobrada); // imprime a lista com os valores dobrados
 let listapar = lista.filter(elemento => elemento % 2 === 0);// filtra os números pares da lista
 console.log(listapar);// imprime a lista com os números pares
 
+let listadobradapar = listapar.map(elemento => elemento * 2);// dobra cada elemento da lista de números pares
+console.log(listadobradapar);// imprime a lista com os valores dobrados dos números pares 
+
 let listamaiorcinco = lista.filter(elemento => elemento > 5);// filtra os números maiores que 5
 console.log(listamaiorcinco);// imprime a lista com os números maiores que 5
 
 let listaencontrar = lista.find(elemento => elemento > 1);//   encontra o primeiro número maior que 1
 console.log(listaencontrar); // imprime o número encontrado
+
