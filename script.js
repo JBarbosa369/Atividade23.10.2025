041a85ff617abb7007a96b1f07c3057b887129e5
JBarbosa369 <jeovana.nascimento@aluno.senai.br>
2025-10-23 10:02:44 -0500

    atividade feita hoje dia 23.10.2025

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-let lista = [1,2,3,4,5,6,7,8,9,10]; // array de números de 1 a 10
-console.log(lista); // imprime a lista original
-let listadobrada = lista.map(elemento => elemento * 2); // dobra cada elemento da lista
-console.log(listadobrada); // imprime a lista com os valores dobrados
+let lista = [1,2,3,4,5,6,7,8,9,10]; //array com 10 elementos
+console.log(lista); //imprime a lista original
+
+let listadobrada = lista.map(elemento => elemento * 2); //multiplica cada elemento da lista por 2
+console.log(listadobrada); //imprime a nova lista com os elementos dobrados
 
 
-let listapar = lista.filter(elemento => elemento % 2 === 0);// filtra os números pares da lista
-console.log(listapar);// imprime a lista com os números pares
+let listapar = lista.filter(elemento => elemento % 2 === 0); //filtra os elementos pares da lista
+console.log(listapar); //imprime a nova lista com os elementos pares
 
-let listadobradapar = lista .map(elemento => elemento * 2);// dobra cada elemento da lista de números pares
-console.log(listadobradapar);// imprime a lista com os valores dobrados dos números pares 
+let listadobradapar = lista .map(elemento => elemento * 2); //dobra cada elemento da lista
+console.log(listadobradapar);
 
-let listamaiorcinco = lista.filter(elemento => elemento > 5);// filtra os números maiores que 5
-console.log(listamaiorcinco);// imprime a lista com os números maiores que 5
+let listamaiorcinco = lista.filter(elemento => elemento > 5);//filtra os elementos maiores que 5
+console.log(listamaiorcinco);
 
-let listaencontrar = lista.find(elemento => elemento > 1);//   encontra o primeiro número maior que 1
-console.log(listaencontrar); // imprime o número encontrado
+let listaencontrar = lista.find(elemento => elemento > 1);//encontra o primeiro elemento maior que 1
+console.log(listaencontrar); 
 
-lista.forEach(elemento => {
+lista.forEach(elemento => { //percorre cada elemento da lista
     console.log(elemento);
-});// imprime cada elemento da lista original usando forEach
+});
 
 
 
-let usuarios = [
+let usuario = [ //array de objetos
     {
         nome: "Jeovana",
         idade: 20
@@ -42,5 +43,18 @@ let usuarios = [
         nome: "alzenir",
         idade: 32
     }
-]
+];
+
+let nomes = usuario.map(elemento => elemento.nome); //cria um novo array com os nomes dos usuarios
+let idades = usuario.map(elemento => elemento.idade); // cria um novo array com as idades dos usuarios
+console.log(nomes);
+console.log(idades);
+
+usuario.forEach(elemento => {
+    console.log(`O nome do usuario é ${elemento.nome} e a idade é ${elemento.idade}`);
+})
+
+
+
+
 
